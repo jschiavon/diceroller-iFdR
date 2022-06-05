ed7915f0d08efef8fd53ae4bc960a2f9b75d29b7
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2022-06-05 10:25:04 +0200

    added average calculator

--- dice_roller.js
+++ dice_roller.js
@@ -1,29 +1,37 @@
+const results = [];
+
 
 function diceroll(sides = 6){
-    var randomNumber = Math.floor(Math.random() * sides) + 1;
+    let randomNumber = Math.floor(Math.random() * sides) + 1;
     return randomNumber;
 }
 
 function rolld20(stat) {
-    var roll = diceroll(20);
-    var modifier = document.getElementById(stat).innerHTML;
+    let roll = diceroll(20);
+    let modifier = document.getElementById(stat).innerHTML;
     if (!modifier){
-        var modifier = 0;
+        var mod = 0;
     } else {
-        var modifier = parseInt(modifier);
+        var mod = parseInt(modifier);
     }
-    var result = roll + modifier;
+    var result = {
+        rolled: roll, 
+        modifier: mod,
+        stat: stat
+    };
+    results.push(result);
     printNumber(result);
-    printDetails(roll, modifier);
-    printHistory(stat, result);
+    printDetails(result);
+    printHistory(results);
+    printAverage(results);
 }
-  
+
 
 
 function applyinputs(){
-    var valori = ["attacco", "secondario", "iniziativa", "armatura", "fisico", "spirito"];
+    let valori = ["attacco", "secondario", "iniziativa", "armatura", "fisico", "spirito"];
 
-    var i, x, y;
+    let i, x, y;
     for (i = 0; i < valori.length; i++){
         x = document.getElementById(valori[i]);
         y = document.getElementById(valori[i] + "_inpt");
@@ -40,26 +48,54 @@ function applyinputs(){
 
 //Prints dice roll to the page
 
-function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
+function printNumber(result) {
+    let placeholder = document.getElementById('placeholder');
+    placeholder.innerHTML = result.rolled + result.modifier;
 }
 
-function printDetails(roll, modifier){
-    var fullresult = document.getElementById('fullresult');
-    if (!modifier){
-        fullresult.innerHTML = roll;
+function printDetails(result) {
+    let fullresult = document.getElementById('fullresult');
+    if (!result.modifier){
+        fullresult.innerHTML = result.rolled;
     } else {
-        fullresult.innerHTML = roll + " + " + modifier;
+        fullresult.innerHTML = result.rolled + " + " + result.modifier;
     }
     
 }
 
-function printHistory(stat, result){
-    var history = document.getElementById('history');
-    history.innerHTML = stat + ": " + result + "\n" + history.innerHTML;
+function printHistory(results) {
+    let history = document.getElementById('history');
+    history.innerHTML = '';
+    for (let i = results.length -1; i >= 0;i--) {
+        history.innerHTML += format_roll(results[i]) + "\n";
+    }
 }
-  
+
+function printAverage(results) {
+    let average = document.getElementById('average');
+    let avg = 0;
+    for (let i = 0; i < results.length; i++) {
+        res = results[i];
+        avg += res.rolled;
+    }
+    avg = avg / results.length;
+    average.innerHTML = Number(avg).toFixed(2);
+}
+
+
+function format_roll(result) {
+    roll = result.rolled;
+    mod = result.modifier;
+    tot = roll + mod;
+    let res = "";
+    if (!mod){
+        res = result.stat + ": " + roll;
+    } else {
+        res = result.stat + ": " + roll + " + " + mod + " = " + tot;
+    }
+    return res;
+}
+
 
 // Get the modal
 var modal = document.getElementById('input_form');
